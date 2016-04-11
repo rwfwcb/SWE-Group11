0232e9493729bed1cecdb564597e6486bccd5cc9
rwfwcb <rwfwcb@mail.missouri.edu>
2016-04-10 21:14:20 -0500

    Added files via upload

--- login-form.php
+++ login-form.php
@@ -9,7 +9,7 @@
    <form class="form-signin" role="form" action="login.php" method="POST">
      <input type="hidden" name="action" value="do_login">
      <h2 class="form-signin-heading">Test login page!</h2>
-     <input type="text" id="username" name="username" class="form-control" placeholder="Username" value="" required autofocus>
+     <input type="text" id="username" name="username" class="form-control" placeholder="Email Address" value="" required autofocus>
      <input type="password" id="password" name="password" class="form-control" placeholder="Password" required>
      <input class="btn btn-lg btn-primary btn-block" type="submit" name="submit" value="Log in">
    </form>
