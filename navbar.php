0e09d8855c944bfb3ee66980e32fd8177d25fdbc
rwfwcb <rwfwcb@mail.missouri.edu>
2016-04-20 18:47:06 -0500

    Added files via upload

--- navbar.php
+++ navbar.php
@@ -17,6 +17,18 @@
       <a class="navbar-brand" href="index.php">LinkedIn</a>
     </div>
     <div id="navbar" class="navbar-collapse collapse">
+
+			<div class="col-sm-3 col-md-3">
+        <form class="navbar-form" role="search">
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="Search" name="q">
+            <div class="input-group-btn">
+                <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+            </div>
+        </div>
+        </form>
+    </div>
+		
       <ul class="nav navbar-nav">
         <li
 		<?php
