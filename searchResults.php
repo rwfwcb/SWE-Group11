3446d7612ed19a90d76c959cd22483575512477e
rwfwcb <rwfwcb@mail.missouri.edu>
2016-04-20 16:30:25 -0500

    Added files via upload

--- searchResults.php
+++ searchResults.php
@@ -18,20 +18,28 @@ if (!$link){
 		printf("Connect failed: %s\n", mysqli_connect_error());
 }
 
+echo "chkpt1 \n";
+
 $input = $_POST['input'];
 $sql = "SELECT id, firstName, lastName FROM Person WHERE firstName LIKE '%?%' OR lastName LIKE '%?%' ";
 
 if ($stmt = mysqli_prepare($link, $sql)){
+	echo "chkpt2 \n";
 	/* bind variables to marker */
 	mysqli_stmt_bind_param($stmt, 'ss', $input, $input);
+	echo "chkpt3 \n";
 	/* execute query */
 	mysqli_stmt_execute($stmt);
+	echo "chkpt4 \n";
 	/* store result */
 	mysqli_stmt_store_result($stmt);
+	echo "chkpt5 \n";
 	/* bind result variables */
 	mysqli_stmt_bind_result($stmt, $id, $firstName, $lastName);
+	echo "chkpt6 \n";
 	/* fetch results */
 	while (mysqli_stmt_fetch($stmt)){
+		echo "chkpt7 \n";
 		echo "<li class = 'list-card'>";
 		echo "<div class='connection-card'>";
 		echo "<div class='connection-body-left'>";
@@ -44,9 +52,11 @@ if ($stmt = mysqli_prepare($link, $sql)){
 		echo "</div>";
 		echo "</div>";
 		echo "</li>";
+		echo "chkpt8 \n";
 	}
 	/* close prepared statement */
 	mysqli_stmt_close($stmt);
 	mysqli_close($link);
+	echo "chkpt9 \n";
 }
 ?>
