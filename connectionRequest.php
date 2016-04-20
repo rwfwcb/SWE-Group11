8850f1d80774607acc7da3d68b2090657974955c
rwfwcb <rwfwcb@mail.missouri.edu>
2016-04-20 00:02:37 -0500

    Added files via upload

--- connectionRequest.php
+++ connectionRequest.php
@@ -43,7 +43,7 @@ if ($stmt4 = mysqli_prepare($link, "INSERT INTO ConnectionRequest VALUES (?, ?)"
 			//echo "<script type='text/javascript'>alert('Connection request sent.')</script>";
 
 		 } else echo "Stmt execute failed.\n";
-	} else echo "id1=$id1, id2=$id2, Bind param failed.\n";
+	} else echo "stmt4=$stmt4, id1=$_POST['id1'], id2=$_POST['id2'], Bind param failed.\n";
 } else echo "Prepared statement failed.\n";
 
 
