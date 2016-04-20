a18d4561386d5435c427c5c5ad45b8e46b53ee5a
rwfwcb <rwfwcb@mail.missouri.edu>
2016-04-20 01:18:29 -0500

    Added files via upload

--- search.php
+++ search.php
@@ -23,7 +23,7 @@ echo "<div class='row'>";
 echo "<div class='col-sm-6 col-sm-offset-3'>";
 echo "<div id='imaginary_container'>";
 echo "<div class='input-group stylish-input-group'>";
-echo "<form class='form-control' action='searchResults.php' method='POST'>";
+echo "<form action='searchResults.php' method='POST'>";
 echo "<input type='text' class='form-control' name='input' placeholder='Search'>";
 echo "<span class='input-group-addon'>";
 echo "<button type='submit'>";
