e377b70a9ccc1c2d6e08203db40c1f719716bb66
rwfwcb <rwfwcb@mail.missouri.edu>
2016-04-19 23:00:03 -0500

    Added files via upload

--- connectionRequest.php
+++ connectionRequest.php
@@ -18,14 +18,14 @@ if (!$link){
 		printf("Connect failed: %s\n", mysqli_connect_error());
 }
 
-$id1 = $_POST['id1'];
-$id2 = $_POST['id2'];
+//$id1 = $_POST['id1'];
+//$id2 = $_POST['id2'];
 
 /* create a prepared statement */
 if ($stmt4 = mysqli_prepare($link, "INSERT INTO ConnectionRequest VALUES (?, ?)")){
 
 	/* bind paramaters to prepared statement */
-	mysqli_stmt_bind_param($stmt4, $id1, $id2);  //{
+	mysqli_stmt_bind_param($stmt4, 1, 10);  //{
 
 		/* execute the query */
 		mysqli_stmt_execute($stmt4);  // {
@@ -33,9 +33,9 @@ if ($stmt4 = mysqli_prepare($link, "INSERT INTO ConnectionRequest VALUES (?, ?)"
 			/* close the prepared statement */
 			mysqli_stmt_close($stmt4);
 
-			echo "<script type='text/javascript'>alert('Connection request sent.')</script>";
-
 			header("Location: index.php?id=profileX");
+
+			echo "<script type='text/javascript'>alert('Connection request sent.')</script>";
 
 		// } else echo "Stmt execute failed.\n";
 	//} else echo "id1=$id1, id2=$id2, Bind param failed.\n";
