472e1e9056a8f51fee1aa7406186e84827d21111
rwfwcb <rwfwcb@mail.missouri.edu>
2016-04-20 12:05:18 -0500

    Added files via upload

--- search.php
+++ search.php
@@ -18,24 +18,24 @@ if (!$link){
 		printf("Connect failed: %s\n", mysqli_connect_error());
 }
 
+echo "<form action='searchResults.php' method='POST'>";
 echo "<div class='container'>";
 echo "<div class='row'>";
 echo "<div class='col-sm-6 col-sm-offset-3'>";
 echo "<div id='imaginary_container'>";
 echo "<div class='input-group stylish-input-group'>";
-//echo "<form action='searchResults.php' method='POST'>";
 echo "<input type='text' class='form-control' name='input' placeholder='Search'>";
 echo "<span class='input-group-addon'>";
 echo "<button type='submit'>";
 echo "<span class='glyphicon glyphicon-search'></span>";
 echo "</button>";
 echo "</span>";
-//echo "</form>";
 echo "</div>";
 echo "</div>";
 echo "</div>";
 echo "</div>";
 echo "</div>";
+echo "</form>";
 
 
 ?>
