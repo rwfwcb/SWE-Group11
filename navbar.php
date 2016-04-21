32f65d09dba5c0c33b55745994326d3755aead37
rwfwcb <rwfwcb@mail.missouri.edu>
2016-04-20 19:17:48 -0500

    Added files via upload

--- navbar.php
+++ navbar.php
@@ -27,6 +27,17 @@
 		<a href="index.php">Home</a></li>
       </ul>
 
+			<div id='searchBar' class="col-sm-3 col-md-3 nav navbar-nav">
+				<form class="navbar-form" role="search">
+				<div class="input-group">
+						<input type="text" class="form-control" placeholder="Search" name="q">
+						<div class="input-group-btn">
+								<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+						</div>
+				</div>
+				</form>
+			</div>
+
         <?php
 			if(!$loggedIn)
 			{ ?>
@@ -59,18 +70,6 @@
                 <li></li>
             </ul>
         <?php } ?>
-
-				<div class="col-sm-3 col-md-3 nav navbar-right">
-	        <form class="navbar-form" role="search">
-	        <div class="input-group">
-	            <input type="text" class="form-control" placeholder="Search" name="q">
-	            <div class="input-group-btn">
-	                <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-	            </div>
-	        </div>
-	        </form>
-	    </div>
-
     </div><!--/.nav-collapse -->
   </div>
 </nav>
