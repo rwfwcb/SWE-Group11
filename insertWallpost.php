26ecb784993f1da6cbf50af97e1150f7c4e3eb25
rwfwcb <rwfwcb@mail.missouri.edu>
2016-04-21 21:10:58 -0500

    Added files via upload

---
+++ insertWallpost.php
@@ -0,0 +1,36 @@
+<?php
+/* start the session */
+session_start();
+
+if(!isset($_SESSION['id'])) {
+	header("Location: index.php?id=login-form");
+}
+
+/* require credentials! */
+require "db.conf";
+
+/* connect to database */
+$link = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+/* check connection */
+if (!$link){
+		printf("Connect failed: %s\n", mysqli_connect_error());
+}
+
+$sql3 = "INSERT INTO Wallpost (id, postTime, body) VALUES (?, ?, ?)";
+/* create a prepared statement */
+if ($stmt3 = mysqli_prepare($link, $sql3)){
+  /* bind variables to marker */
+	$id = $_SESSION['id']
+	$now = "now()"
+  $body = $_POST['wallpost'];
+
+  mysqli_stmt_bind_param($stmt3, "sss", $id, $now, $body) or die("bind param");
+  /* execute query */
+  mysqli_stmt_execute($stmt3);
+	/* close the prepared statement */
+	mysqli_stmt_close($stmt3);
+	
+} else echo "<script type='text/javascript'>alert('Prepared statement 3 failed.')</script>";
+
+?>
