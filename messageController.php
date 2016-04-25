a172b748cea5c2937acf5129b95f7ae04c77bb59
rwfwcb <rwfwcb@mail.missouri.edu>
2016-04-25 00:08:59 -0500

    Added files via upload

--- messageController.php
+++ messageController.php
@@ -20,16 +20,16 @@ if (!$link){
 
 /* run prepared queries to see if the user has any connections*/
 	/* create a prepared statement */
-		if($stmt = mysqli_prepare($link, "INSERT INTO Message (senderID, receiverID, body, messageWhen)VALUES (?, ?, ?, now())")){
+		if($stmt = mysqli_prepare($link, "INSERT INTO Message VALUES (?, ?, ?, now())")){
 		/* bind variables to marker */
-		mysqli_stmt_bind_param($stmt, 'sss', $_SESSION['id'], $_POST['receiver'], $_POST['messageContent']);
+		mysqli_stmt_bind_param($stmt, 'dds', $_SESSION['id'], $_POST['receiver'], $_POST['messageContent']);
 		/* execute query */
 		mysqli_stmt_execute($stmt);
 		/* close the prepared stmt */
     mysqli_stmt_close($stmt);
     /* close the connection */
     mysqli_close($link);
-  }
+  } else echo "Prepared stmt failed.\n";
 
 header("Location: index.php?id=messenger");
 ?>
