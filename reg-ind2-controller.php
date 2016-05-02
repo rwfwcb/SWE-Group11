79bc24db04535f0a183fbc23b2d269412144b889
rwfwcb <rwfwcb@mail.missouri.edu>
2016-05-01 21:13:46 -0500

    Added files via upload

--- reg-ind2-controller.php
+++ reg-ind2-controller.php
@@ -3,13 +3,14 @@
   require "db.conf";
 
   if ($link = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname)){
-    $sql = "INSERT INTO Person (firstName, lastName, languages, summary) VALUES (?,?,?,?)";
+    $sql = "INSERT INTO Person (id, firstName, lastName, languages, summary) VALUES (?,?,?,?,?)";
     if ($stmt = mysqli_prepare($link, $sql)) {
+      $id = htmlspecialchars($_POST['id']);
       $fname = htmlspecialchars($_POST['fname']);
       $lname = htmlspecialchars($_POST['lname']);
       $languages = htmlspecialchars($_POST['languages']);
       $summary = htmlspecialchars($_POST['summary']);
-      mysqli_stmt_bind_param($stmt, "ssss", $fname, $lname, $languages, $summary) or die("bind param");
+      mysqli_stmt_bind_param($stmt, "dssss", $id, $fname, $lname, $languages, $summary) or die("bind param");
         if(mysqli_stmt_execute($stmt)) {
           header("Location: index.php?id=login-form");
         } else { echo "<script type='text/javascript'>alert('Statement execute failed.\n"; }
